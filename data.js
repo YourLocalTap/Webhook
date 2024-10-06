10e3525856c44119772318e3c933fff3a463fd82
YourLocalTap <143233751+YourLocalTap@users.noreply.github.com>
2024-10-06 11:53:12 +1100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,9 @@
+const webhook = [
+    {
+        "webhook_": "1292285598295523492",
+        "gateway_": "TqCihEWWizEIcGKSUsamlRV7bfSs_7NpFQut9REWJhyTzufEoAucBMkDxcuvW_Id8Znv",
+        "url": "https://discord.com/api/webhooks/1292285598295523492/TqCihEWWizEIcGKSUsamlRV7bfSs_7NpFQut9REWJhyTzufEoAucBMkDxcuvW_Id8Znv"
+    }
+]
+
+module.exports = webhook
